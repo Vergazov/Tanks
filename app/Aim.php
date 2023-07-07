88cdf76ce3e2946b81dd11efc516cd4f6bb726e3
Vergazov_A <vergazka@gmail.com>
2023-07-07 15:21:58 +0300

    добавил классы Aim и Tank

--- app/Aim.php
+++ app/Aim.php
@@ -3,5 +3,14 @@
 
 class Aim
 {
+    private int $armorThickness;
 
+    public function __construct($armorThickness)
+    {
+        $this->armorThickness = $armorThickness;
+    }
+
+    public function getArmorThickness(){
+        return $this->armorThickness;
+    }
 }
--- app/Tank.php
+++ app/Tank.php
@@ -3,5 +3,30 @@
 
 class Tank
 {
+    private string $name;
+    private int $maxSpeed;
+    private int $armorPiercing;
+
+    public function __construct($name,  $maxSpeed, $armorPiercing)
+    {
+        $this->name - $name;
+        $this->maxSpeed - $maxSpeed;
+        $this->armorPiercing - $armorPiercing;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getMaxSpeed(): int
+    {
+        return $this->maxSpeed;
+    }
+
+    public function getArmorPiercing(): int
+    {
+        return $this->armorPiercing;
+    }
 
 }
