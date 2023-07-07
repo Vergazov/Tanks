d354b831ee5189a047139ad80c0f75761946456f
Vergazov_A <vergazka@gmail.com>
2023-07-07 15:25:11 +0300

    инициализировал мишени

--- Collector.php
+++ Collector.php
@@ -3,5 +3,17 @@
 require_once 'app/Aim.php';
 require_once 'app/Dispatcher.php';
 
+$aims = [
+    new Aim(2),
+    new Aim(7),
+    new Aim(5),
+    new Aim(15),
+    new Aim(30),
+    new Aim(1),
+    new Aim(54),
+    new Aim(12),
+    new Aim(61)
+];
+
 
 
