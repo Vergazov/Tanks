a450bbfcd54e9b2d9be4fc03e9439f82a5853330
Vergazov_A <vergazka@gmail.com>
2023-07-13 10:28:40 +0300

    рабочий вариант

--- app/Sorter.php
+++ app/Sorter.php
@@ -6,21 +6,18 @@ class Sorter
 {
     public function sortBySpeed($rightTanks)
     {
-        usort($rightTanks, static function ($first, $second){
-            {
-                if ($first === $second) {
-                    return 0;
-                }
-                if ($first < $second) {
-                    return -1;
-                }
-                if ($first > $second) {
-                    return 1;
-                }
+        usort($rightTanks, static function ($first, $second)
+        {
+            if ($first === $second) {
+                return 0;
+            }
+            if ($first < $second) {
+                return -1;
+            }
+            if ($first > $second) {
+                return 1;
             }
         } );
         return $rightTanks;
-
+        }
     }
-
-}
